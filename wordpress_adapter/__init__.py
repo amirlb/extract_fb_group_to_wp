f7fb1c46ca448aec47f08b40d182a26614007701
Amir Livne Bar-on <amir.livne.baron@gmail.com>
2016-06-07 15:17:21 +0300

    handle duplicate comments, fix formatting, allow attachments and uploads to server

--- wordpress_adapter/__init__.py
+++ wordpress_adapter/__init__.py
@@ -1,10 +1,12 @@
 import html
+import mimetypes
 import unicodedata
 import time
 import datetime
 import pytz
 import wordpress_xmlrpc
-from wordpress_xmlrpc.methods import posts, comments
+from wordpress_xmlrpc import xmlrpc_client
+from wordpress_xmlrpc.methods import posts, comments, media
 
 DEFAULT_TEXT_DIRECTION = 'rtl'
 GROUP_TIMEZONE = pytz.timezone('Asia/Jerusalem')
@@ -30,7 +32,7 @@ def format_message(text):
     divs = []
     for line in lines:
         if len(line.strip()) == 0:
-            divs.append('')
+            divs.append('<br />')
         else:
             line_bidi = unicodedata.bidirectional(line.strip()[0])
             if line_bidi == 'L':
@@ -38,7 +40,7 @@ def format_message(text):
             elif line_bidi == 'R':
                 direction = 'rtl'
             divs.append(div_with_direction(line, direction))
-    return '&nbsp;\n'.join(divs)
+    return '\n'.join(divs)
 
 
 def facebook_timestamp_to_datetime(timestamp):
@@ -62,21 +64,36 @@ def extract_title(message):
 
 class WordPressAdapter(object):
 
-    def __init__(self, blog_name, username, password, debug=False):
-        rpc_url = 'https://{}.wordpress.com/xmlrpc.php'.format(blog_name)
+    def __init__(self, blog_url, username, password, debug=False):
+        if blog_url.endswith('/'):
+            rpc_url = '{}xmlrpc.php'.format(blog_url)
+        else:
+            rpc_url = '{}/xmlrpc.php'.format(blog_url)
         self._client = wordpress_xmlrpc.Client(rpc_url, username, password)
         self._debug = debug
 
-    def post_from_fb(self, fb_dict):
-        # TODO: upload images and attachments to wordpress
-        # TODO: also handle attachments
+    def upload(self, file_name):
+        data = {'name': file_name,
+                'type': mimetypes.guess_type(file_name)[0],
+                'bits': xmlrpc_client.Binary(open(file_name, 'rb').read())}
+        result = self._client.call(media.UploadFile(data))
+        return result['url']
+
+    def post_from_fb(self, fb_dict, ul_resources=False):
         post = wordpress_xmlrpc.WordPressPost()
         post.title = extract_title(fb_dict['message'])
         post.content = format_message(fb_dict['message'])
         if fb_dict['pictures']:
+            if ul_resources:
+                fb_dict['pictures'] = [self.upload(f) for f in fb_dict['pictures']]
             images = ''.join('<img src="{}" />\n'.format(url) for url in fb_dict['pictures'])
-            post.content = images + post.content
-            # post.thumbnail = fb_dict['pictures'][0]
+            post.content = images + '<br />\n' + post.content
+        if fb_dict['attachments']:
+            if ul_resources:
+                fb_dict['attachments'] = [(name, self.upload(f)) for name, f in fb_dict['attachments']]
+            attachments = ''.join('<div><a href="{}">{}</a></div>\n'.format(url, name)
+                                  for name, url in fb_dict['attachments'])
+            post.content += '<br />\n<div>Attachments:</div>\n' + attachments
         post.date = facebook_timestamp_to_datetime(fb_dict['created_time'])
         if fb_dict['updated_time'] != fb_dict['created_time']:
             post.date_modified = facebook_timestamp_to_datetime(fb_dict['updated_time'])
@@ -86,9 +103,9 @@ class WordPressAdapter(object):
         if self._debug:
             print('posting')
         post_id = self._client.call(posts.NewPost(post))
-        self.add_comments(post_id, post_id, fb_dict['comments'])
+        self.add_comments(post_id, post_id, fb_dict['comments'], ul_resources)
 
-    def add_comments(self, post_id, parent, fb_comments):
+    def add_comments(self, post_id, parent, fb_comments, ul_resources=False):
         for fb_dict in fb_comments:
             comment = wordpress_xmlrpc.WordPressComment()
             comment.post = post_id
@@ -101,13 +118,23 @@ class WordPressAdapter(object):
             comment.author_ip = '10.0.0.0'
             comment.content = format_message(fb_dict['message'])
             if 'attachment' in fb_dict:
+                if ul_resources:
+                    fb_dict['attachment'] = self.upload(fb_dict['attachment'])
                 if self._debug:
                     print('image {}'.format(fb_dict['attachment']))
-                comment.content += '\n<br /><img src={}" \>'.format(fb_dict['attachment'])
+                comment.content += '\n<br /><img src="{}" />'.format(fb_dict['attachment'])
             if self._debug:
                 if parent == post_id:
                     print('comment')
                 else:
                     print('- comment')
-            comment_id = self._client.call(comments.NewComment(post_id, comment))
-            self.add_comments(post_id, comment_id, fb_dict['comments'])
+            try:
+                comment_id = self._client.call(comments.NewComment(post_id, comment))
+                self.add_comments(post_id, comment_id, fb_dict['comments'], ul_resources)
+            except xmlrpc_client.Fault as e:
+                if e.faultCode == 409:
+                    if self._debug:
+                        print('(duplicate)')
+                    pass
+                else:
+                    raise
